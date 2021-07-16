88aed458bed01fc9927eb14843c24e3457800610
Joe Christensen <joechristensenwork@gmail.com>
2021-07-16 11:51:49 -0700

    commit tree e69a748773801d4014dbb22b5fa649765048d2be

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    let strArr = string.split("");
+    strArr[0].toUpperCase();
+    return strArr.join("");
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-']+/g, '');
+  }
+
+  static titleize(string) {
+    let strArr = string.split(" ");
+    strArr.forEach((word) => {
+      let exclude = ["a", "an", "but", "of", "and", "for", "at", "by", "from"];
+
+      if(!exclude.includes(word)) {
+        let strArr2 = word.split("");
+        strArr2[0].toUpperCase();
+        word = strArr2.join("");
+      }
+    });
+    return strArr.join(" ");
+  }
 }
